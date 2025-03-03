c04d709bd18e53d8f7c2e2d96cb72e67707761b9
Buster Daemon <Buster Daemon@bugsbunnygdi@pm.me>
2025-03-03 18:56:54 +0300

    Удалены ненужные функции

--- lab1/affineTransform.h
+++ lab1/affineTransform.h
@@ -1,8 +1,6 @@
 #ifndef _AFFINE_TRANSFORM_H_
 #define _AFFINE_TRANSFORM_H_
 
-void simpleTransform(int af_matrix[3][3], int startX, int startY, int* destX, int* destY);
-void rotateTransform(int af_matrix[3][3], int startX, int startY, int* destX, int* destY);
 float* simpleTransformMove(float matrix[3][3], float coords[3]);
 float* simpleTransformScale(float matrix[3][3], float coords[3]);
 float* simpleTransformRotate(float matrix[3][3], float angle, float coords[3]);
